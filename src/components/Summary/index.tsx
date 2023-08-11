c89d714cc03fae3188fd6d458a66dcf6e6e92ce5
Adriano Misina <adriano.misina@gmail.com>
2023-08-11 08:22:58 -0300

    criação hook para o resumo

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,32 +1,12 @@
-import { useContext } from "react"
 import { SummaryCard, SummaryContainer } from "./styles"
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react"
-import { TransactionsContext } from "../../contexts/TransactionContext"
 import { priceFormatter } from "../../utils/formatter"
+import { useSummary } from "../../hooks/useSummary"
 
 const Summary = () => {
-  const { transactions } = useContext(TransactionsContext)
   
-  //{income: 0, outcome: 0, total: 0}
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-      return acc;
-    },
+  const summary = useSummary()
 
-    {
-      income: 0,
-      outcome: 0,
-      total: 0
-    }
-  )
-  
   return (
     <SummaryContainer>
       <SummaryCard>
